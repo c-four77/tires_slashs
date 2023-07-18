2747d0c87bd76554a80d3f5525eccdb7628a1ffb
CFOUR <116594943+c-four77@users.noreply.github.com>
2023-07-18 20:06:54 +0100

    tires_slash

---
+++ tires_slash/fxmanifest.lua
@@ -0,0 +1,11 @@
+--## MoruQ Developer ##--
+
+fx_version 'cerulean'
+game 'gta5'
+
+
+
+server_script "server.lua"
+client_script "client.lua"
+
+lua54 'yes'
---
+++ tires_slash/server.lua
@@ -0,0 +1,6 @@
+--## MoruQ Developer ##--
+
+RegisterServerEvent("SlashTires:TargetClient")
+AddEventHandler("SlashTires:TargetClient", function(client, tireIndex)
+	TriggerClientEvent("SlashTires:SlashClientTire", client, tireIndex)
+end)
